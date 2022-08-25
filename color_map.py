8bc5dd6c10c610dda27a6f6183c961f2a21dde7c
Jonathan <jolin3apa@gmail.com>
2022-08-26 01:33:03 +0900

    update color map

--- color_map.py
+++ color_map.py
@@ -6,25 +6,25 @@ COLOR_DESCRETE_MAP = {
     "OTHERS": "grey",
     "七瀬千夏": "red",
     "天音ゆめ": "lavender",
-    "恵深あむ": "yellow",
+    "恵深あむ": "red",
     "楠木りほ": "cyan",
-    "大西春菜": "blue",
+    "雅春奈": "red",
     "椎名まどか": "cyan",
     "中谷亜優": "white",
     "石田綾音": "fuchsia",
     "濱崎みき": "white",
-    "瀬乃悠月": "crimson",
-    "望月紗奈": "pink",
-    "音羽のあ": "blue",
+    "瀬乃悠月": "red",
+    "天使 さな": "yellow",
+    "のえる": "purple",
     "火野快飛": "azure",
     "メグ・ピッチ・オリオン": "lime",
     "コイヌ フユ": "yellow",
-    "涼乃みほ": "lightblue",
+    "涼乃みほ": "cyan",
     "松島朱里": "green",
     "星名 夢音": "lightgreen",
     "雨宮れいな": "white",
     "岬あやめ": "yellow",
-    "鳴上綺羅": "lightblue",
+    "鳴上綺羅": "cyan",
     "桜衣みゆな": "pink",
     "Joyce": "green",
     "木戸怜緒奈": "cyan",
@@ -33,7 +33,9 @@ COLOR_DESCRETE_MAP = {
     "南 歩唯": "pink",
     "昊乃ひな": "orange",
     "日向なの": "orange",
-    "きゃりー": "yellow"
+    "きゃりー": "yellow",
+    "もしかして、るか": "yellow"
+
 }
 
 
