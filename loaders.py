684ebff40c96679faa6db8282c7d06fb34434c7e
Jonathan <jolin3apa@gmail.com>
2023-02-19 14:22:33 +0900

    update data cache

--- loaders.py
+++ loaders.py
@@ -7,7 +7,7 @@ gc = get_google_conn()
 
 
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600, allow_output_mutation=True)
+@st.cache_data(ttl=600)
 def get_worksheet(url, sheet_num=0):
     worksheet = gc.open_by_url(url).get_worksheet(sheet_num)
     df = pd.DataFrame(worksheet.get_all_values())
@@ -17,7 +17,7 @@ def get_worksheet(url, sheet_num=0):
     return df
 
 
-@st.cache(ttl=600, allow_output_mutation=True)
+@st.cache_data(ttl=600)
 def get_worksheet_location(url, sheet_num=3):
     worksheet = gc.open_by_url(url).get_worksheet(sheet_num)
     df = pd.DataFrame(worksheet.get_all_values())
@@ -27,7 +27,7 @@ def get_worksheet_location(url, sheet_num=3):
     return df
 
 
-@st.cache(ttl=600, allow_output_mutation=True)
+@st.cache_data(ttl=600)
 def get_worksheet_names(url, sheet_num=1):
     worksheet = gc.open_by_url(url).get_worksheet(sheet_num)
     df = pd.DataFrame(worksheet.get_all_values())
