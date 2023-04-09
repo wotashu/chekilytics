ae583cea5fd4fbca4f1931793bce6309fe20badb
Jonathan <jolin3apa@gmail.com>
2023-04-09 22:02:35 +0900

    simplify layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,12 +32,9 @@ def main():
 
     names_df = names_df[names_df.date.between(first_date, last_date)]
 
-    groupby_select = st.selectbox(
-        "Choose Columns to group by",
-        ("name", "cheki_id"),
-    )
+    name_tab, cheki_tab = st.tabs(["💃name", "🎴cheki"])
 
-    if "name" in groupby_select:
+    with name_tab:
         also_group_by_date = st.checkbox("Also group by date?", value=False)
         if also_group_by_date:
             groupby_select = ["date", "name"]
@@ -45,31 +42,31 @@ def main():
         else:
             groupby_select = ["name"]
             sort_level = 1
-        df = munge.get_records_df(
+        name_df = munge.get_records_df(
             names_df=names_df,
             person_df=person_df,
             groupby_select=groupby_select,
             sort_level=sort_level,
         )
 
-        df.columns = [str(col) for col in df.columns]
+        name_df.columns = [str(col) for col in name_df.columns]
         if selected_persons:
-            df = df[df["name"].isin(selected_persons)]
+            name_df = name_df[name_df["name"].isin(selected_persons)]
 
         chart_tab, data_tab = st.tabs(["📈 Chart", "🗃 Data"])
 
         with data_tab:
-            st.dataframe(df, 800, 800)
+            st.dataframe(name_df, 800, 800)
 
         with chart_tab:
-            max_value = int(df["total"].max())
+            max_value = int(name_df["total"].max())
 
             col1, col2 = st.columns(2)
 
             with col1:
                 cutoff_value = 0
                 if len(selected_persons) >= 12:
-                    cutoff_value = round(df["total"].median())
+                    cutoff_value = round(name_df["total"].median())
                 cutoff = st.number_input(
                     "Cutoff for other",
                     min_value=0,
@@ -77,78 +74,89 @@ def main():
                     value=cutoff_value,
                 )
                 if cutoff > 0:
-                    df = munge.get_cutoff_data(df, cutoff)
+                    name_df = munge.get_cutoff_data(name_df, cutoff)
             with col2:
                 top_n = st.number_input(
-                    "Keep top n", step=1, min_value=0, max_value=len(df) + 1, value=0
+                    "Keep top n",
+                    step=1,
+                    min_value=0,
+                    max_value=len(name_df) + 1,
+                    value=0,
                 )
 
             if top_n:
-                df = df.head(top_n)
+                name_df = name_df.head(top_n)
 
             treemap_tab, bar_tab, pie_tab = st.tabs(["🌳treemap", "📊bar", "🥧pie"])
             with treemap_tab:
-                fig = figures.get_treemap_fig(df)
+                fig = figures.get_treemap_fig(name_df)
                 st.plotly_chart(fig)
             with bar_tab:
-                fig = figures.get_bar_fig(df)
+                fig = figures.get_bar_fig(name_df)
                 st.plotly_chart(fig)
             with pie_tab:
-                fig = figures.get_pie_fig(df)
+                fig = figures.get_pie_fig(name_df)
                 st.plotly_chart(fig)
 
-    elif groupby_select == "cheki_id":
-        df = dated_cheki_df
-        df = df[
-            (df.date >= pd.to_datetime(first_date))
-            & (df.date <= pd.to_datetime(last_date))
+    with cheki_tab:
+        dated_cheki_df = dated_cheki_df[
+            (dated_cheki_df.date >= pd.to_datetime(first_date))
+            & (dated_cheki_df.date <= pd.to_datetime(last_date))
         ]
-        df["date"] = df["date"].dt.strftime("%Y-%m-%d")
-        df = pd.merge(df, venue_df, how="left", left_on="location", right_on="location")
+        dated_cheki_df["date"] = dated_cheki_df["date"].dt.strftime("%Y-%m-%d")
+        dated_cheki_df = pd.merge(
+            dated_cheki_df,
+            venue_df,
+            how="left",
+            left_on="location",
+            right_on="location",
+        )
 
         if selected_persons:
             temp_dfs = []
-            name_cols = [col for col in df.columns if "name" in col]
+            name_cols = [col for col in dated_cheki_df.columns if "name" in col]
             for col in name_cols:
                 for person in selected_persons:
-                    temp_dfs.append(df[df[col].str.contains(person)])
-            df = pd.concat(temp_dfs)
+                    temp_dfs.append(
+                        dated_cheki_df[dated_cheki_df[col].str.contains(person)]
+                    )
+            dated_cheki_df = pd.concat(temp_dfs)
 
-            df = df.replace("", np.nan)
-            df = df.dropna(how="all", axis=1)
-            df = df.replace(np.nan, "")
-            df.sort_values("date", inplace=True)
+            dated_cheki_df = dated_cheki_df.replace("", np.nan)
+            dated_cheki_df = dated_cheki_df.dropna(how="all", axis=1)
+            dated_cheki_df = dated_cheki_df.replace(np.nan, "")
+            dated_cheki_df.sort_values("date", inplace=True)
 
         tab1, tab2 = st.tabs(["🗺️ Map", "🗃 Data"])
 
         with tab2:
-            st.dataframe(df, 800, 800)
+            st.dataframe(dated_cheki_df, 800, 800)
 
         with tab1:
-            logger.debug(f"DF preview: {df.head()}")
-            logger.debug(f"DF columns: {df.columns}")
-            logger.debug(f"DF shape: {df.shape}")
+            logger.debug(f"DF preview: {dated_cheki_df.head()}")
+            logger.debug(f"DF columns: {dated_cheki_df.columns}")
+            logger.debug(f"DF shape: {dated_cheki_df.shape}")
 
             df_groupby = (
-                df.groupby("location")["date"]
+                dated_cheki_df.groupby("location")["date"]
                 .count()
                 .reset_index()
                 .rename(columns={"date": "count"})
             )
-            df = pd.merge(
+            cheki_map_df = pd.merge(
                 df_groupby,
-                df[["location", "latitude", "longitude", "full_address"]],
+                dated_cheki_df[["location", "latitude", "longitude", "full_address"]],
                 how="inner",
                 on="location",
             )
             # df = df[["location", "latitude", "longitude", "full_address"]]
-            df = df.replace("", np.nan)
-            df = df.dropna(axis=0, how="any")
-            df.drop_duplicates(inplace=True)
+            cheki_map_df = cheki_map_df.replace("", np.nan)
+            cheki_map_df = cheki_map_df.dropna(axis=0, how="any")
+            cheki_map_df.drop_duplicates(inplace=True)
             # df = df.reset_index(drop=True)
 
-            df["latitude"] = df["latitude"].astype(float)
-            df["longitude"] = df["longitude"].astype(float)
+            cheki_map_df["latitude"] = cheki_map_df["latitude"].astype(float)
+            cheki_map_df["longitude"] = cheki_map_df["longitude"].astype(float)
 
             (
                 heat_tab,
@@ -157,19 +165,21 @@ def main():
             ) = st.tabs(["🔥Heatmap", "🏢Column", "⭕Scatter"])
 
             with heat_tab:
-                deck = get_map_layer(df=df, map_type="heat")
+                deck = get_map_layer(df=cheki_map_df, map_type="heat")
                 st.pydeck_chart(deck)
             with column_tab:
-                deck = get_map_layer(df=df, map_type="column")
+                deck = get_map_layer(df=cheki_map_df, map_type="column")
                 st.pydeck_chart(deck)
             with scatter_tab:
-                deck = get_map_layer(df=df, map_type="scatter")
+                deck = get_map_layer(df=cheki_map_df, map_type="scatter")
                 st.pydeck_chart(deck)
 
-            st.dataframe(df, use_container_width=True)
-            st.write(f"Total values: {len(df)}, count: {df['count'].sum()}")
+            st.dataframe(cheki_map_df, use_container_width=True)
+            st.write(
+                f"Total values: {len(cheki_map_df)}, count: {cheki_map_df['count'].sum()}"
+            )
 
-    st.write(f"Total values: {len(df)}")
+    st.write(f"Total values: {len(dated_cheki_df)}")
 
 
 if __name__ == "__main__":
